08bba4f9a390168759f815e60271d9bee344c4b7
Avishekdevnath <avishekdevnath@gmail.com>
2023-10-18 20:49:17 +0600

    first commit

---
+++ C programming/Outsbook/Volume 1/P101.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int salary, annual_income;
+    
+    scanf("%d",&salary);
+    
+    annual_income = salary * 365;
+    
+    printf("%d\n",annual_income);
+
+    return 0;
+}
